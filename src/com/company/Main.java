ed8fe92c19014df67a625e6a1d4474f8d4ab54c0
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 21:56:14 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        ReadSortWrite rsw = new ReadSortWrite(
-                "C:\\Users\\Kotes\\Desktop\\java_labs_kotes\\lab5\\in.txt",
-                "C:\\Users\\Kotes\\Desktop\\java_labs_kotes\\lab5\\out.txt");
+    public static void main(String[] args){
     }
 }
